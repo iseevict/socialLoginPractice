84a48738106bcbbbd86951cd4faa0f9a3e9c489e
stpark0059 <stpark0509@gmail.com>
2024-08-14 16:59:50 +0900

    start

---
+++ src/main/java/socialLoginPractice/iseevictP1/IseevictP1Application.java
@@ -0,0 +1,13 @@
+package socialLoginPractice.iseevictP1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IseevictP1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IseevictP1Application.class, args);
+	}
+
+}
---
+++ src/test/java/socialLoginPractice/iseevictP1/IseevictP1ApplicationTests.java
@@ -0,0 +1,13 @@
+package socialLoginPractice.iseevictP1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IseevictP1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
